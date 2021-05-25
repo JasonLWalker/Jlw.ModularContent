c1f5c5f5234f9be39d16f9650899d061f38fc126
JasonLWalker <jason@draxjinn.com>
2021-05-25 08:44:50 -0500

    Added LocalDB and sample website overrides

---
+++ Jlw.Web.Core31.LocalizedContent.SampleWebApp/Controllers/ContentOverrideAdminApiController.cs
@@ -0,0 +1,36 @@
+﻿using Jlw.Data.LocalizedContent;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jlw.Web.Core31.LocalizedContent.SampleWebApp.Controllers
+{
+    [Authorize("ContentOverrideAdmin")]
+    [Route("admin/ContentOverride/api/")]
+    public class ContentOverrideAdminApiController : Jlw.Web.Rcl.LocalizedContent.Areas.LocalizedContentField.Controllers.ApiController
+    {
+        public ContentOverrideAdminApiController(ILocalizedContentFieldRepository repository) : base (repository)
+        {
+            _groupFilter = "%";
+        }
+
+        public override object DtList(LocalizedContentFieldDataTablesInput o)
+        {
+            return GetDataTableList(o);
+        }
+
+        public override object Data(LocalizedContentFieldRecordInput o)
+        {
+            return GetRecordData(o);
+        }
+
+        public override object Save(LocalizedContentFieldRecordInput o)
+        {
+            return SaveRecordData(o);
+        }
+
+        public override object Delete(LocalizedContentFieldRecordInput o)
+        {
+            return DeleteRecordData(o);
+        }
+    }
+}
--- Jlw.Web.Core31.LocalizedContent.SampleWebApp/Startup.cs
+++ Jlw.Web.Core31.LocalizedContent.SampleWebApp/Startup.cs
@@ -5,17 +5,99 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Jlw.Utilities.Data.DbUtility;
+using MartinCostello.SqlLocalDb;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Jlw.Extensions.Identity.Mock;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using TUser = Jlw.Extensions.Identity.Stores.ModularBaseUser;
 
 namespace Jlw.Web.Core31.LocalizedContent.SampleWebApp
 {
     public class Startup
     {
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Env { get; set; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            Env = env;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            AddMockedUsers();
+            services.AddIdentityMocking<TUser>();
+
+
+            services.AddSingleton<IModularDbClient>(new ModularDbClient<SqlConnection, SqlCommand, SqlParameter, SqlConnectionStringBuilder>());
+            services.AddSqlLocalDB(options =>
+            {
+                options.AutomaticallyDeleteInstanceFiles = true;
+                options.StopTimeout = TimeSpan.FromMinutes(1);
+            });
+            services.AddSqlLocalDbInstance(options =>
+            {
+                options.InstanceName = Configuration.GetConnectionString("LocalDbInstanceName");
+            });
+
+            var provider = services.BuildServiceProvider();
+            var dbInstanceInfo = provider.GetRequiredService<ISqlLocalDbInstanceInfo>();
+            var connString = dbInstanceInfo.GetConnectionString();
+            services.AddLocalizedContentFieldRepository(options => options.ConnectionString = connString);
+            services.AddLocalizedContentTextRepository(options => options.ConnectionString = connString);
+            services.AddLocalizedGroupDataItemRepository(options => options.ConnectionString = connString);
+
+            var mvcBuilder = services.AddControllersWithViews();
+
+            if (Env.IsDevelopment())
+            {
+                mvcBuilder.AddRazorRuntimeCompilation();
+            }
+
+            mvcBuilder.AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.IgnoreNullValues = true;
+            });
+
+            
+            mvcBuilder.AddNewtonsoftJson(o =>
+            {
+                /*
+                o.SerializerSettings.Converters.Add(new StringEnumConverter());
+                o.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                */
+            });
+            
+
+            services.AddLocalizedContentAdmin();
+            services.AddAuthorization(options =>
+            {
+                options.AddDefaultLocalizedContentAdminAuthorizationPolicy();
+
+                options.AddPolicy("ContentOverrideAdmin", policy =>
+                {
+                    policy.RequireAssertion(context =>
+                    {
+                        return context.User.Claims.Any(claim => claim.Type.Equals("ContentOverrideAccess", StringComparison.InvariantCultureIgnoreCase));
+                    });
+                });
+
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +108,102 @@ namespace Jlw.Web.Core31.LocalizedContent.SampleWebApp
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
+
+            app.UseAuthentication();    // Initializes MS Identity Authentication Middleware
+            app.UseAuthorization();     // Initialized MS Identity Authorization Middleware
+
+            app.UseSqlLocalDbInstance();
+
+            app.UseDefaultLocalizedContentAdmin();
+
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                /*
+                endpoints.MapControllerRoute(
+                    name: "OverrideFieldAdmin",
+                    pattern: "Admin/OverrideField/{groupKey?}/{parentKey?}",
+                    defaults: new { Controller = "OverrideFieldAdmin", Action = "Index" },
+                    constraints: new { Controller = "Admin", Action = "Index" });
+                */
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
+        }
+
+        protected void AddMockedUsers()
+        {
+            MockUserStore<TUser>.AddMockedUser(
+                new TUser(new
+                {
+                    Id = 1,
+                    UserName = "testuser@draxjinn.info",
+                    NormalizedUserName = "testuser@draxjinn.info",
+                    Email = "testuser@draxjinn.info",
+                    NormalizedEmail = "testuser@draxjinn.info",
+                    PasswordHash = "test",
+                    EmailConfirmed = true
+                }),
+                new[]
+                {
+                    new IdentityUserClaim<string>()
+                    {
+                        ClaimType = "LocalizedContentAccess",
+                        ClaimValue = "Read",
+                    }
+                });
+
+            MockUserStore<TUser>.AddMockedUser(
+                new TUser(new
+                {
+                    Id = 2,
+                    UserName = "testadmin@draxjinn.info",
+                    NormalizedUserName = "testadmin@draxjinn.info",
+                    Email = "testadmin@draxjinn.info",
+                    NormalizedEmail = "testadmin@draxjinn.info",
+                    PasswordHash = "test",
+                    EmailConfirmed = true
+                }),
+                new[]
+                {
+                    new IdentityUserClaim<string>()
+                    {
+                        ClaimType = "ContentOverrideAccess",
+                        ClaimValue = "Read",
+                    }
+                });
+
+            MockUserStore<TUser>.AddMockedUser(
+                new TUser(new
+                {
+                    Id = 3,
+                    UserName = "testsuper@draxjinn.info",
+                    NormalizedUserName = "testsuper@draxjinn.info",
+                    Email = "testsuper@draxjinn.info",
+                    NormalizedEmail = "testsuper@draxjinn.info",
+                    PasswordHash = "test",
+                    EmailConfirmed = true
+                }),
+                new[]
+                {
+                    new IdentityUserClaim<string>()
+                    {
+                        ClaimType = "LocalizedContentAccess",
+                        ClaimValue = "Read",
+                    },
+                    new IdentityUserClaim<string>()
+                    {
+                        ClaimType = "AdminAccess",
+                        ClaimValue = "Standard20",
+                    }
+                });
+
         }
     }
 }
