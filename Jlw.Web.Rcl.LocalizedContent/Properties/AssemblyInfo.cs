2ccd7810cf852134050efd28515b7f166dd5ae92
JasonLWalker <jason@draxjinn.com>
2021-05-26 09:29:53 -0500

    added automated build versioning

---
+++ Jlw.Web.Rcl.LocalizedContent/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jlw.Web.Rcl.LocalizedContent")]
+[assembly: AssemblyDescription("Razor Class Library to implement form and content creation via a database-driven UI")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jason L. Walker")]
+[assembly: AssemblyProduct("Jlw.LocalizedContent")]
+[assembly: AssemblyCopyright("Copyright ©2019-2021 Jason L. Walker")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("765917de-f725-487a-94d3-fced26163d5f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.1.*")]
