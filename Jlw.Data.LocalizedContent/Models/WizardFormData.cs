8bb3642bf8c8358984253cef63a9e0aa90e061b0
JasonLWalker <jason@draxjinn.com>
2021-06-01 12:47:30 -0500

    Add Naming Strategy for JSON serialization

--- Jlw.Data.LocalizedContent/Models/WizardFormData.cs
+++ Jlw.Data.LocalizedContent/Models/WizardFormData.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using Jlw.Utilities.Data;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace Jlw.Data.LocalizedContent
 {
     public class WizardFormData : WizardContentField
     {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, NamingStrategyType = typeof(DefaultNamingStrategy))]
+        [JsonConverter(typeof(JlwJsonConverter<bool>))]
         public bool UseCardLayout { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, NamingStrategyType = typeof(DefaultNamingStrategy))]
         public WizardButtonData EditButton { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, NamingStrategyType = typeof(DefaultNamingStrategy))]
         public IEnumerable<JToken> Fields { get; set; } = new List<JToken>();
 
         public WizardFormData(string formKey, IEnumerable<WizardContentField> fieldData, WizardButtonData editButton = null)
