77720a23336753f95752bbef80c667804434d8b2
JasonLWalker <jason@draxjinn.com>
2021-06-21 12:08:22 -0500

    Set to pack on build and simplified build script

--- build-scripts/build.ps1
+++ build-scripts/build.ps1
@@ -11,14 +11,16 @@ if (-Not ($packageName)){
 # Install dependencies
 #dotnet restore
 
-dotnet pack Jlw.Data.LocalizedContent --version-suffix=$versionSuffix --configuration $buildType
+#dotnet pack Jlw.Data.LocalizedContent --version-suffix=$versionSuffix --configuration $buildType
 
-dotnet build Jlw.Data.LocalizedContent.Tests --version-suffix=$versionSuffix --configuration $buildType
+#dotnet build Jlw.Data.LocalizedContent.Tests --version-suffix=$versionSuffix --configuration $buildType
 
 # Build with dotnet
 # dotnet build --version-suffix=$versionSuffix --configuration $buildType
 
 # Pack Nuget package
-dotnet pack Jlw.Web.Rcl.LocalizedContent --version-suffix=$versionSuffix --configuration $buildType
+#dotnet pack Jlw.Web.Rcl.LocalizedContent --version-suffix=$versionSuffix --configuration $buildType
 
-dotnet build Jlw.Web.Core31.LocalizedContent.SampleWebApp --version-suffix=$versionSuffix --configuration $buildType
+#dotnet build Jlw.Web.Core31.LocalizedContent.SampleWebApp --version-suffix=$versionSuffix --configuration $buildType
+
+dotnet build --version-suffix=$versionSuffix --configuration $buildType
