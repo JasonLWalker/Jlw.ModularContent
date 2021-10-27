2fb122c5f8f29813d050ac600d936237f045a707
JasonLWalker <jason@draxjinn.com>
2021-10-27 15:54:15 -0500

    refactored to allow override

---
+++ Jlw.Web.Core31.LocalizedContent.SampleWebApp/Controllers/OverrideLocalizedContentLanguageApiController.cs
@@ -0,0 +1,24 @@
+﻿using Jlw.Data.LocalizedContent;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jlw.Web.Core31.LocalizedContent.SampleWebApp.Controllers
+{
+    [Authorize("ContentOverrideAdmin")]
+    [Route("admin/api/OverrideLocalizedContentLanguage/")]
+    public class OverrideLocalizedContentLanguageApiController : Jlw.Web.Rcl.LocalizedContent.Areas.LocalizedGroupDataItem.Controllers.ApiController
+    {
+        public OverrideLocalizedContentLanguageApiController(ILocalizedGroupDataItemRepository repository) : base (repository)
+        {
+            _groupFilter = "LocalizedContentLanguages";
+            _forcedGroupKey = "LocalizedContentLanguages";
+            _unlockApi = true;
+        }
+
+        public override object SaveRecordData(LocalizedGroupDataItemRecordInput o)
+        {
+            o.Language = string.IsNullOrWhiteSpace(o.Language) ? "EN" : o.Language;
+            return base.SaveRecordData(o);
+        }
+    }
+}
