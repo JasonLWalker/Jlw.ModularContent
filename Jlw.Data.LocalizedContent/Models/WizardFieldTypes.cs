3c18c7ee224fada7535e79b7ac36677a2ddcf9f5
JasonLWalker <jason@draxjinn.com>
2022-02-17 12:16:07 -0600

    add enums for consistency of naming

---
+++ Jlw.Data.LocalizedContent/Models/WizardFieldTypes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jlw.Data.LocalizedContent
+{
+    public enum WizardFieldTypes
+    {
+        WIZARD,
+        SCREEN,
+        EMAIL,
+        SUBJECT,
+        BODY,
+        HEADING,
+        HTML,
+        FORM,
+        EMBED,
+        BUTTON,
+        INPUT,
+        SELECT,
+        TEXTAREA,
+        SEPARATOR
+    }
+}
---
+++ Jlw.Data.LocalizedContent/Models/WizardInputTypes.cs
@@ -0,0 +1,23 @@
+﻿namespace Jlw.Data.LocalizedContent
+{
+    public enum WizardInputTypes
+    {
+        DATE,
+        DATETIMELOCAL,
+        MONTH,
+        TIME,
+        WEEK,
+        COLOR,
+        CHECKBOX,
+        HIDDEN,
+        RADIO,
+        RANGE,
+        TEXT,
+        PASSWORD,
+        TEL,
+        URL,
+        EMAIL,
+        NUMBER,
+        SEARCH
+    }
+}
