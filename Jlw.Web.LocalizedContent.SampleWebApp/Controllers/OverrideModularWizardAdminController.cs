e752ff21de202e6bf2bba08fb30f637721505166
JasonLWalker <jason@draxjinn.com>
2022-04-11 09:51:43 -0500

    Switch from ViewData to Model for settings

--- Jlw.Web.LocalizedContent.SampleWebApp/Controllers/OverrideModularWizardAdminController.cs
+++ Jlw.Web.LocalizedContent.SampleWebApp/Controllers/OverrideModularWizardAdminController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
 
 namespace Jlw.Web.LocalizedContent.SampleWebApp.Controllers
 {
@@ -8,19 +11,23 @@ namespace Jlw.Web.LocalizedContent.SampleWebApp.Controllers
     [Authorize("ContentOverrideAdmin")]
     public class OverrideModularWizardAdminController : Jlw.Web.Rcl.LocalizedContent.Areas.ModularWizardAdmin.Controllers.AdminController
     {
+        public OverrideModularWizardAdminController(LinkGenerator linkGenerator) : base()
+        {
+            DefaultSettings.ApiOverrideUrl = linkGenerator.GetPathByAction("Index", "OverrideModularWizardAdminApi", new { Area = "" });
+            DefaultSettings.ToolboxHeight = "calc(100vh - 48px)";
+        }
+
         [HttpGet]
         public override ActionResult Index()
         {
-            string adminApiUrl = Url.Action("Index", "OverrideModularWizardAdminApi", new { Area = "" });
-
             var aBreadcrumb = new List<KeyValuePair<string, string>>()
             {
                 new KeyValuePair<string, string>(Url.Action("Index", "Home"), "Home"),
                 new KeyValuePair<string, string>(Url.Action("Index", "Admin"), "Administration"),
             };
 
+            //DefaultSettings.ApiOverrideUrl = Url.Action("Index", "OverrideModularWizardAdminApi", new { Area = "" });
             ViewData["Breadcrumb"] = aBreadcrumb;
-            ViewData["apiOverrideUrl"] = adminApiUrl;
 
             return base.Index();
         }
