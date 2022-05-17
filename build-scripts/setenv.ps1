2dd5d0524f6e39516e4238c7f5975a6295a50f0c
JasonLWalker <jason@draxjinn.com>
2022-05-17 09:29:59 -0500

    add quotes to environment variables.

--- build-scripts/setenv.ps1
+++ build-scripts/setenv.ps1
@@ -18,7 +18,7 @@ if ($ENV:GITHUB_ENV) {
     "DOTNET_CLI_TELEMETRY_OPTOUT=1" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
     "DOTNET_MULTILEVEL_LOOKUP=1" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
     "BUILD_VERSION=$vPrefix$vSuffix" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-    "BUILD_VERSION_PREFIX=$vPrefix" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-    "BUILD_VERSION_SUFFIX=$vSuffix" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-    "PKGVERSION=$vPrefix$vSuffix" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+    "BUILD_VERSION_PREFIX='$vPrefix'" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+    "BUILD_VERSION_SUFFIX='$vSuffix'" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+    "PKGVERSION='$vPrefix$vSuffix'" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
 }
