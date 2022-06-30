3216be118b69d399e00c2e50f3e5adb51bbbfb94
JasonLWalker <jason@draxjinn.com>
2022-06-30 12:17:16 -0500

    Fix Unit text for model

--- Jlw.Web.Rcl.LocalizedContent.Tests/ModelTests/TestSchema/ModularWizardAdmin_WizardAdminSettingsTestSchema.cs
+++ Jlw.Web.Rcl.LocalizedContent.Tests/ModelTests/TestSchema/ModularWizardAdmin_WizardAdminSettingsTestSchema.cs
@@ -36,6 +36,7 @@ namespace Jlw.Web.Rcl.LocalizedContent.Tests.ModelTests
             AddProperty(typeof(string), "ApiOverrideUrl", Public, Public);
             AddProperty(typeof(string), "JsRoot", Public, Public);
             AddProperty(typeof(string), "ToolboxHeight", Public, Public);
+            AddProperty(typeof(string), "HiddenFilterPrefix", Public, Public);
             AddProperty(typeof(List<SelectListItem>), "LanguageList", Public, null);
         }
 
