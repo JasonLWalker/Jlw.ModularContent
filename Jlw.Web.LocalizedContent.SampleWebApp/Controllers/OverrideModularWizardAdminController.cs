cfb8e349ad0a68c4e1762343fff416f41e4c5f1c
JasonLWalker <jason@draxjinn.com>
2022-07-08 15:47:24 -0500

    refactor admin controllers to use settings from constructor

--- Jlw.Web.LocalizedContent.SampleWebApp/Controllers/OverrideModularWizardAdminController.cs
+++ Jlw.Web.LocalizedContent.SampleWebApp/Controllers/OverrideModularWizardAdminController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Jlw.Data.LocalizedContent;
+using Jlw.Web.Rcl.LocalizedContent;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -15,43 +16,9 @@ namespace Jlw.Web.LocalizedContent.SampleWebApp.Controllers
     public class OverrideModularWizardAdminController : Jlw.Web.Rcl.LocalizedContent.Areas.ModularWizardAdmin.Controllers.AdminController
     {
         
-        public OverrideModularWizardAdminController(LinkGenerator linkGenerator, IWizardFactoryRepository repository) : base()
+        public OverrideModularWizardAdminController(IWizardAdminSettings settings, IWizardFactoryRepository repository) : base(settings, repository)
         {
             _groupFilter = "Sample%";
-
-            DataRepository = repository;
-
-            DefaultSettings.ApiOverrideUrl = linkGenerator.GetPathByAction("Index", "OverrideModularWizardAdminApi", new { Area = "" });
-            DefaultSettings.ToolboxHeight = "calc(100vh - 58px)";
-            DefaultSettings.ShowSideNav = true;
-            //DefaultSettings.SideNavDefault = false;
-            DefaultSettings.ShowWireFrame = true;
-            DefaultSettings.IsAdmin = true;
-            DefaultSettings.CanEdit = true;
-            DefaultSettings.CanInsert = true;
-            DefaultSettings.CanDelete = true;
-            DefaultSettings.HiddenFilterPrefix = "Sample";
-            DefaultSettings.LanguageList.Add(new SelectListItem("Chinese", "CN"));
-            DefaultSettings.TinyMceSettings = JObject.Parse(@"
-                {
-                    useTinyMce: true,
-                    plugins: 'image imagetools paste link code help',
-                    height: 'calc(90vh - 350px)',
-                    image_advtab: true,
-                    paste_data_images: true,
-                    toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | code ',
-                    menu: {
-                        file: { title: 'File', items: 'newdocument ' },
-                        edit: { title: 'Edit', items: 'undo redo | cut copy paste | selectall | searchreplace' },
-                        view: { title: 'View', items: 'code | visualaid visualchars visualblocks | spellchecker | preview fullscreen' },
-                        insert: { title: 'Insert', items: 'image link media template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor toc | insertdatetime' },
-                        format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | formats blockformats fontformats fontsizes align lineheight | forecolor backcolor | removeformat' },
-                        tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | code wordcount' },
-                        table: { title: 'Table', items: 'inserttable | cell row column | tableprops deletetable' },
-                        help: { title: 'Help', items: 'help' }
-                    }
-                }");
-
         }
 
         [HttpGet]
