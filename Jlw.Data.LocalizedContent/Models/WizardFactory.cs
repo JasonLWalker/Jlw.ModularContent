66b51bc3bde2707519f85996d48f188c9b832ebb
JasonLWalker <jason@draxjinn.com>
2022-08-10 13:19:38 -0500

    Resolve FieldData for screens

--- Jlw.Data.LocalizedContent/Models/WizardFactory.cs
+++ Jlw.Data.LocalizedContent/Models/WizardFactory.cs
@@ -289,6 +289,7 @@ namespace Jlw.Data.LocalizedContent
 
                     screenField.Body = ResolvePlaceholders(screenField.Body, replacementObject);
                     screenField.Heading = ResolvePlaceholders(screenField.Heading, replacementObject);
+                    screenField.FieldData = ResolveDataPlaceholders(JToken.FromObject(screenField.FieldData), replacementObject);
 
                     if (screenField.Forms?.Count() > 0)
                     {
