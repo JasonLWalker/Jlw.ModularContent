6aba92ec6a7891798720f76a2f9e734264df37fb
JasonLWalker <jason@draxjinn.com>
2022-10-28 14:08:19 -0500

    Add export feature. Fix misc bugs. Resolves: #178 Resolves: #164

--- Jlw.Web.Rcl.LocalizedContent/Areas/ModularWizardAdmin/Controllers/AdminController.cs
+++ Jlw.Web.Rcl.LocalizedContent/Areas/ModularWizardAdmin/Controllers/AdminController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using Jlw.Data.LocalizedContent;
 using Jlw.Utilities.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json.Linq;
 
 namespace Jlw.Web.Rcl.LocalizedContent.Areas.ModularWizardAdmin.Controllers
@@ -17,7 +22,6 @@ namespace Jlw.Web.Rcl.LocalizedContent.Areas.ModularWizardAdmin.Controllers
         protected AdminController(IWizardAdminSettings settings, IWizardFactoryRepository repository)
         {
             DataRepository = repository;
-
             DefaultSettings.IsAdmin = settings.IsAdmin;
             DefaultSettings.CanEdit = settings.CanEdit;
             DefaultSettings.CanDelete = settings.CanDelete;
@@ -30,6 +34,7 @@ namespace Jlw.Web.Rcl.LocalizedContent.Areas.ModularWizardAdmin.Controllers
             DefaultSettings.ExtraCss = settings.ExtraCss;
             DefaultSettings.ExtraScript = settings.ExtraScript;
             DefaultSettings.Area = settings.Area;
+            DefaultSettings.ControllerName = settings.ControllerName;
             DefaultSettings.JsRoot = settings.JsRoot;
             DefaultSettings.ToolboxHeight = settings.ToolboxHeight;
             DefaultSettings.HiddenFilterPrefix = settings.HiddenFilterPrefix;
@@ -47,6 +52,7 @@ namespace Jlw.Web.Rcl.LocalizedContent.Areas.ModularWizardAdmin.Controllers
             DefaultSettings.ApiOverrideUrl = settings.ApiOverrideUrl;
             DefaultSettings.PreviewUrl = settings.PreviewUrl;
             DefaultSettings.PreviewScreenUrl = settings.PreviewScreenUrl;
+            DefaultSettings.ExportUrl = settings.ExportUrl;
 
         }
 
@@ -66,6 +72,71 @@ namespace Jlw.Web.Rcl.LocalizedContent.Areas.ModularWizardAdmin.Controllers
             var settings = new WizardPreviewSettings(DefaultSettings);
             settings.SideNav.Add(new WizardSideNavItem(new WizardContentField(new {Label = "Instructions", FieldType="SCREEN"})));
             return GetViewResult("Preview", settings);
+        }
+
+        [HttpGet("Export/{wizardName?}/{screenName?}")]
+        public virtual ActionResult Export(string wizardName = null, string screenName = null)
+        {
+            StringBuilder sb = new StringBuilder();
+            IEnumerable<ILocalizedContentField> fieldData;
+            if (string.IsNullOrWhiteSpace(screenName))
+                fieldData = DataRepository.GetWizardFields(wizardName, _groupFilter);
+            else
+                fieldData = DataRepository.GetWizardFields(wizardName, screenName, "EN", _groupFilter);
+
+            IEnumerable<ILocalizedContentText> aLangValues = DataRepository.GetLanguageValues(wizardName);
+            IEnumerable<ILocalizedContentText> aErrorValues = DataRepository.GetLanguageValues(wizardName + "_Errors");
+            List<ILocalizedContentText> aLanguageFields = new List<ILocalizedContentText>();
+
+            foreach (var field in fieldData)
+            {
+                var langRange = aLangValues?.Where(x => (x.FieldKey.Equals(field.FieldKey) && x.ParentKey.Equals(field.ParentKey)))?.ToArray();
+
+                if (langRange?.Any() ?? false)
+                    aLanguageFields.AddRange(langRange);
+
+                langRange = aErrorValues?.Where(x => (x.ParentKey.Equals(field.FieldKey)))?.ToArray();
+
+                if (langRange?.Any() ?? false)
+                    aLanguageFields.AddRange(langRange);
+
+                sb.AppendLine($"INSERT INTO [dbo].[LocalizedContentFields] ([GroupKey], [FieldKey], [FieldType], [FieldData], [FieldClass], [ParentKey], [DefaultLabel], [WrapperClass], [WrapperHtmlStart], [WrapperHtmlEnd], [AuditChangeType], [AuditChangeBy], [AuditChangeDate], [Order]) VALUES (" +
+                              $"{(field.GroupKey == null ? "null" : $"N'{field.GroupKey.Replace("'", "''")}'")}, " +
+                              $"{(field.FieldKey == null ? "null" : $"N'{field.FieldKey.Replace("'", "''")}'")}, " +
+                              $"{(field.FieldType == null ? "null" : $"N'{field.FieldType.Replace("'", "''")}'")}, " +
+                              $"{(field.FieldData == null ? "null" : $"N'{field.FieldData.Replace("'", "''")}'")}, " +
+                              $"{(field.FieldClass == null ? "null" : $"N'{field.FieldClass.Replace("'", "''")}'")}, " +
+                              $"{(field.ParentKey == null ? "null" : $"N'{field.ParentKey.Replace("'", "''")}'")}, " +
+                              $"{(field.DefaultLabel == null ? "null" : $"N'{field.DefaultLabel.Replace("'", "''")}'")}, " +
+                              $"{(field.WrapperClass == null ? "null" : $"N'{field.WrapperClass.Replace("'", "''")}'")}, " +
+                              $"{(field.WrapperHtmlStart == null ? "null" : $"N'{field.WrapperHtmlStart.Replace("'", "''")}'")}, " +
+                              $"{(field.WrapperHtmlEnd == null ? "null" : $"N'{field.WrapperHtmlStart.Replace("'", "''")}'")}, " +
+                              $"{(field.AuditChangeType == null ? "null" : $"N'{field.AuditChangeType.Replace("'", "''")}'")}, " +
+                              $"{(field.AuditChangeBy == null ? "null" : $"N'{field.AuditChangeBy.Replace("'", "''")}'")}, " +
+                              $"N'{field.AuditChangeDate.ToString("yyyy-MM-dd HH:mm:ss")}', " +
+                              $"N'{field.Order}'" +
+                              $");");
+            }
+
+            foreach (var field in aLanguageFields)
+            {
+                sb.AppendLine($"INSERT INTO [dbo].[LocalizedContentText] ([GroupKey], [FieldKey], [Language], [Text], [AuditChangeType], [AuditChangeBy], [AuditChangeDate], [ParentKey]) VALUES (" +
+                              $"{(field.GroupKey == null ? "null" : $"N'{field.GroupKey.Replace("'", "''")}'")}, " +
+                              $"{(field.FieldKey == null ? "null" : $"N'{field.FieldKey.Replace("'", "''")}'")}, " +
+                              $"{(field.Language == null ? "null" : $"N'{field.Language.Replace("'", "''")}'")}, " +
+                              $"{(field.Text == null ? "null" : $"N'{field.Text.Replace("'", "''")}'")}, " +
+                              $"{(field.AuditChangeType == null ? "null" : $"N'{field.AuditChangeType.Replace("'", "''")}'")}, " +
+                              $"{(field.AuditChangeBy == null ? "null" : $"N'{field.AuditChangeBy.Replace("'", "''")}'")}, " +
+                              $"N'{field.AuditChangeDate.ToString("yyyy-MM-dd HH:mm:ss")}', " +
+                              $"{(field.ParentKey == null ? "null" : $"N'{field.ParentKey.Replace("'", "''")}'")}" +
+                              $");");
+            }
+                
+            byte[] aBytes = Encoding.ASCII.GetBytes(sb.ToString());
+            return new FileContentResult(aBytes, "text/plain")
+            {
+                FileDownloadName = $"{wizardName}-{DateTime.Now.Ticks}.sql"
+            };
         }
 
         [HttpGet("PreviewScreen/{wizardName?}/{screenName?}")]
