c980417304416edf542f8d4f3087372f25284949
JasonLWalker <jason@draxjinn.com>
2023-05-12 16:10:23 -0500

    clean up code

--- Jlw.Web.Rcl.LocalizedContent/Areas/ModularWizardEmailAdmin/Controllers/AdminController.cs
+++ Jlw.Web.Rcl.LocalizedContent/Areas/ModularWizardEmailAdmin/Controllers/AdminController.cs
@@ -34,49 +34,16 @@ namespace Jlw.Web.Rcl.LocalizedContent.Areas.ModularWizardEmailAdmin.Controllers
 
         public class WizardAdminSettings : LocalizedContent.WizardAdminSettings
         {
-            //public bool IsAdmin { get; set; }
-            //public bool CanEdit { get; set; }
-            //public bool CanDelete { get; set; }
-            //public bool CanInsert { get; set; }
-
-            //public bool UseWysiwyg { get; set; } = true;
-            //public bool ShowWireFrame { get; set; }
-            //public bool SideNavDefault { get; set; }
-            //public bool WireFrameDefault { get; set; }
             public bool ShowGroup { get; set; } = false;
             public bool ShowFieldName { get; set; } = false;
             public bool ShowLanguage { get; set; } = false;
             public bool ShowParent { get; set; } = false;
             public bool ShowKey { get; set; } = true;
-            //public JToken TinyMceSettings { get; set; }
-            //public string DefaultWizard { get; }
-            //public string Version { get; }
-            //public string GroupFilter { get; }
-            //public bool ShowSideNav { get; set; }
-            //public WizardSideNav SideNav { get; }
-            //public string PageTitle { get; set; }
-            //public string ExtraCss { get; set; }
-            //public string ExtraScript { get; set; }
-            //public string Area { get; set; }
-            //public string ApiControllerName { get; set; }
-            //public string ControllerName { get; set; }
-            //public LinkGenerator LinkGenerator { get; set; }
-            //public string AdminUrl { get; set; }
-            //public string PreviewUrl { get; set; }
-            //public string PreviewScreenUrl { get; set; }
-            //public string ExportUrl { get; set; }
-            //public string ToolboxHeight { get; set; }
-            //public string HiddenFilterPrefix { get; set; }
-            //public object PreviewRecordData { get; set; }
-            //public string ApiOverrideUrl { get; set; }
-            //public string JsRoot { get; set; }
 
             public string GroupKey { get; set; }
             public string FieldKey { get; set; }
             public string ParentKey { get; set; }
             public string Language { get; set; }
-
-            //public List<SelectListItem> LanguageList { get; } = new List<SelectListItem>() { new SelectListItem("English", "EN") };
 
             public string DataTableClass { get; set; } = "table table-bordered table-sm table-striped table-hover";
             public bool DataTablePaging { get; set; } = true;
@@ -87,7 +54,9 @@ namespace Jlw.Web.Rcl.LocalizedContent.Areas.ModularWizardEmailAdmin.Controllers
             public string EditView { get; set; } = "~/Areas/ModularWizardEmailAdmin/Views/Admin/_EditRecord.cshtml";
             public string DataTableListView { get; set; } = "~/Areas/ModularWizardEmailAdmin/Views/Admin/_DataTableList.cshtml";
             public string DataTableScriptView { get; set; } = "~/Areas/ModularWizardEmailAdmin/Views/Admin/_DataTableListScript.cshtml";
-        }
 
+            public WizardAdminSettings() : this(null) {}
+            public WizardAdminSettings(object o) : base(o) { }
+        }
     }
 }
